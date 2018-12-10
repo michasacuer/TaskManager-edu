4097cb3ee70041c892f114be54574217ac30b41a
Michał Szewczak <michasacuer3@gmail.com>
2018-12-10 21:51:59 +0100

    refactor

--- TaskManager/ViewModels/ActiveTaskViewModel.cs
+++ TaskManager/ViewModels/ActiveTaskViewModel.cs
@@ -1,5 +1,9 @@
 ﻿using Caliburn.Micro;
 using TaskManager.Models;
+using System.Timers;
+using System;
+using System.Diagnostics;
+using System.Threading;
 
 namespace TaskManager.ViewModels
 {
@@ -7,16 +11,21 @@ namespace TaskManager.ViewModels
     {
         public string ActiveTaskTextBlock { get; set; }
         public string DescriptionTextBlock { get; set; }
+        public string TimerActiveTaskTextBlock { get; set; }
 
         public ActiveTaskViewModel(Task task)
         {
+            stopwatch.Start();
             activeTask = task;
             ActiveTaskTextBlock = $"{task.TaskName}, Priorytet: {task.Priority}";
             DescriptionTextBlock = task.Description;
+            TimerActiveTaskTextBlock = stopwatch.Elapsed.ToString();
+            NotifyOfPropertyChange(() => TimerActiveTaskTextBlock);
         }
 
         public void CancelTaskButton() => TryClose();
 
+        private Stopwatch stopwatch = new Stopwatch();
         private Task activeTask;
     }
 }
