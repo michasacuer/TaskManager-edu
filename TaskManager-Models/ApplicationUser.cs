9cb30413e95d25fac152ad0082af175e6c6d1c1e
Michał Szewczak <michasacuer3@gmail.com>
2019-04-28 13:03:42 +0200

    WebApi DbContext setup, added User.cs in Models

---
+++ TaskManager-Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskManager.Models
+{
+    using Microsoft.AspNetCore.Identity;
+
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+
+        public string SecondName { get; set; }
+    }
+}
--- TaskManager-WebApi/Startup.cs
+++ TaskManager-WebApi/Startup.cs
@@ -3,8 +3,11 @@
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
+    using TaskManager.Models;
+    using TaskManager.WebApi.Models;
 
     public class Startup
     {
@@ -19,6 +22,11 @@
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<TaskManagerDbContext>(
+                options => options.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentityCore<ApplicationUser>().AddEntityFrameworkStores<TaskManagerDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,6 +41,7 @@
                 app.UseHsts();
             }
 
+            app.UseAuthentication();
             app.UseHttpsRedirection();
             app.UseMvc();
             app.UseCors(builder => builder.AllowAnyHeader().AllowAnyHeader().AllowAnyOrigin());
