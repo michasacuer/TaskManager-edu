3fe168c26023b3b885fd4cf384c1367952b3125e
michasacuer <michasacuer3@gmail.com>
2019-05-17 08:27:54 +0200

    Account service mock

---
+++ TaskManager-WebApi/Services/AccountService.cs
@@ -0,0 +1,41 @@
+﻿namespace TaskManager.WebApi.Services
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.Extensions.Configuration;
+    using TaskManager.Models;
+
+    public class AccountService : IAccountService
+    {
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        private readonly UserManager<ApplicationUser> userManager;
+
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        private readonly IConfiguration configuration;
+
+        public AccountService(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            RoleManager<IdentityRole> roleManager,
+            IConfiguration configuration)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.roleManager = roleManager;
+            this.configuration = configuration;
+        }
+
+        public async Task<object> Login(object loginBindingModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<object> Register(object registrationBindingModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TaskManager-WebApi/Services/IAccountService.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskManager.WebApi.Services
+{
+    using System.Threading.Tasks;
+
+    public interface IAccountService
+    {
+        Task<object> Login(object loginBindingModel);
+
+        Task<object> Register(object registrationBindingModel);
+    }
+}
