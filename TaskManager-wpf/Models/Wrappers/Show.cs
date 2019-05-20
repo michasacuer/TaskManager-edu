2948af8e97f703d612134ca4c489cc896232dc61
michasacuer <michasacuer3@gmail.com>
2019-05-20 07:41:35 +0200

    Shwo refactor, deleted unused file

--- TaskManager-wpf/Models/Wrappers/Show.cs
+++ TaskManager-wpf/Models/Wrappers/Show.cs
@@ -9,11 +9,9 @@
 
         public static void SuccesBox(string alert) => manager.ShowDialog(new SuccesBoxViewModel(alert), null, null);
 
-        public static void RegistrationBox()
-            => manager.ShowDialog(new RegistrationViewModel(), null, null);
+        public static void RegistrationBox() => manager.ShowDialog(new RegistrationViewModel(), null, null);
 
-        public static void LoginBox(LoggedUser loggedUser)
-            => manager.ShowDialog(new LoginViewModel(loggedUser), null, null);
+        public static void LoginBox(LoggedUser loggedUser) => manager.ShowDialog(new LoginViewModel(loggedUser), null, null);
 
         public static void ActiveTaskBox(TaskManager.Models.Task task, string projectName)
             => manager.ShowDialog(new ActiveTaskViewModel(task, projectName), null, null);
