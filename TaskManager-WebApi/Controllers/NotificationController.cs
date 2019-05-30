23dfff854074b16cf64626cb747557c6e0388a71
Michał Szewczak <michasacuer3@gmail.com>
2019-05-30 20:16:01 +0200

    notifications controller

---
+++ TaskManager-WebApi/Controllers/NotificationController.cs
@@ -0,0 +1,27 @@
+﻿namespace TaskManager_WebApi.Controllers
+{
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using TaskManager.Models;
+    using TaskManager.WebApi.Services;
+
+    [Route("[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly INotificationService notificationService;
+
+        public NotificationController(INotificationService notificationService)
+        {
+            this.notificationService = notificationService;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IEnumerable<Notification> GetNotifications()
+        {
+            return this.notificationService.GetList();
+        }
+    }
+}
