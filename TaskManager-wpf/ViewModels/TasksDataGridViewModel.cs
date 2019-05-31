e04d2787653c4817efc479359ded067a371e2312
wojtekw1996 <wojjtekk1996@gmail.com>
2019-05-31 13:34:31 +0200

    delete button

--- TaskManager-wpf/ViewModels/TasksDataGridViewModel.cs
+++ TaskManager-wpf/ViewModels/TasksDataGridViewModel.cs
@@ -1,7 +1,9 @@
 ﻿namespace TaskManager.WPF.ViewModels
 {
     using System.Collections.Generic;
+    using System.Threading.Tasks;
     using Caliburn.Micro;
+    using TaskManager.WPF.Helpers;
     using TaskManager.WPF.Models;
 
     public class TasksDataGridViewModel : Screen
@@ -12,5 +14,13 @@
         }
 
         public List<TaskManager.Models.Task> Tasks { get; set; }
+
+        public async Task DeleteButton(TaskManager.Models.Task task)
+        {
+            await this.TryCloseAsync();
+
+            Show.
+            Show.SuccesBox("Task usunięto pomyślnie!");
+        }
     }
 }
