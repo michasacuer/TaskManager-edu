c68b3f756cdddb45f6e38e6256630c6bf429cb50
wojtekw1996 <wojjtekk1996@gmail.com>
2019-06-01 17:00:11 +0200

    datagrids refreshing

--- TaskManager-wpf/ViewModels/TasksDataGridViewModel.cs
+++ TaskManager-wpf/ViewModels/TasksDataGridViewModel.cs
@@ -5,6 +5,7 @@
     using Caliburn.Micro;
     using TaskManager.WPF.Helpers;
     using TaskManager.WPF.Models;
+    using TaskManager.WPF.Services;
 
     public class TasksDataGridViewModel : Screen
     {
@@ -15,16 +16,32 @@
 
         public List<TaskManager.Models.Task> Tasks { get; set; }
 
-        public void InfoButton(TaskManager.Models.Task task)
+        public async void InfoButton(TaskManager.Models.Task task)
         {
             Show.InfoTaskBox(task);
+
+            await Repository.Instance.FetchAll();
+
+            var httpDataService = new HttpDataService();
+            var temp = await httpDataService.Get<TaskManager.Models.Task>();
+            this.Tasks = (List<TaskManager.Models.Task>)temp;
+
+            this.NotifyOfPropertyChange(() => this.Tasks);
         }
 
-        public void DeleteButton(TaskManager.Models.Task task)
+        public async void DeleteButton(TaskManager.Models.Task task)
         {
             if (LoggedUser.Instance.HavePermissionToDelete())
             {
                 Show.DeleteTaskBox(task);
+                 
+                await Repository.Instance.FetchAll();
+
+                var httpDataService = new HttpDataService();
+                var temp = await httpDataService.Get<TaskManager.Models.Task>();
+                this.Tasks = (List<TaskManager.Models.Task>)temp;
+
+                this.NotifyOfPropertyChange(() => this.Tasks);
             }
             else
             {
