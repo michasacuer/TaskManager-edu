343aa9a416ce5555eee6f9c250ce58e3108a2686
Michał Szewczak <michasacuer3@gmail.com>
2019-06-01 08:30:01 +0200

    new column in ended task - spent story points

---
+++ TaskManager-WebApi/Migrations/20190601062936_added_spentStoryPoints_in_endedTask.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TaskManager.WebApi.Migrations
+{
+    public partial class added_spentStoryPoints_in_endedTask : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SpentStoryPoints",
+                table: "EndedTasks",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SpentStoryPoints",
+                table: "EndedTasks");
+        }
+    }
+}
