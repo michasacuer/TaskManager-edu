30c264788cbb28bc59e55b494016f13c708d1a84
michasacuer <michasacuer3@gmail.com>
2019-06-10 17:29:21 +0200

    helper refactor

--- TaskManager-wpf/Helpers/ManagerPanelHelper.cs
+++ TaskManager-wpf/Helpers/ManagerPanelHelper.cs
@@ -1,6 +1,7 @@
 ﻿namespace TaskManager.WPF.Helpers
 {
     using System.Linq;
+    using System.Threading.Tasks;
     using IronPdf;
     using TaskManager.Models;
     using TaskManager.WPF.Models;
@@ -10,10 +11,7 @@
     {
         public async void GeneratePdf(string projectName, string filepath)
         {
-            var projectId = Repository.Instance.Projects.Single(p => p.Name == projectName).Id;
-
-            var httpDataService = new HttpDataService();
-            var pdf = await httpDataService.Get<Pdf>(projectId);
+            var pdf = await this.GetPdf(projectName);
 
             var htmlToPdf = new HtmlToPdf();
             htmlToPdf.RenderHtmlAsPdf(pdf.Content).SaveAs(filepath);
@@ -23,18 +21,23 @@
 
         public async void PrintRaport(string projectName)
         {
-            var projectId = Repository.Instance.Projects.Single(p => p.Name == projectName).Id;
-
-            var httpDataService = new HttpDataService();
-            var html = await httpDataService.Get<Pdf>(projectId);
+            var pdf = await this.GetPdf(projectName);
 
             var htmlToPdf = new HtmlToPdf();
-            var pdf = htmlToPdf.RenderHtmlAsPdf(html.Content);
+            var render = htmlToPdf.RenderHtmlAsPdf(pdf.Content);
 
-            var printDocument = pdf.GetPrintDocument();
+            var printDocument = render.GetPrintDocument();
 
             var fileDialog = new FileDialog();
             fileDialog.OpenPrinterDialog(printDocument);
+        }
+
+        private async Task<Pdf> GetPdf(string projectName)
+        {
+            var projectId = Repository.Instance.Projects.Single(p => p.Name == projectName).Id;
+
+            var httpDataService = new HttpDataService();
+            return await httpDataService.Get<Pdf>(projectId);
         }
     }
 }
